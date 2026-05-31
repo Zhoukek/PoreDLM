ef060a12cecb03819b2c17bf830db0e3b487e318
zhoukexuan <1280721629@qq.com>
2026-05-31 15:14:41 +0800

    add OLMo_DLM #3

--- src/poredlm/data/stage3_OLMo_DLM/step01_dolma.sh
+++ src/poredlm/data/stage3_OLMo_DLM/step01_dolma.sh
@@ -1,15 +1,15 @@
 # 定义公共参数
-TOKENIZER_PATH="/mnt/si003067jezr/default/poregpt/dolma/tokenizers/pore_64k/tokenizer.json"
-BASE_SRC="/mnt/zzbnew/rnamodel/zhoukexuan/poregpt/dataset/human_dna_032g_split1280_overlap256_baseline"
-BASE_DST="/mnt/zzbnew/rnamodel/zhoukexuan/poregpt/dataset/human_dna_032g_split1280_overlap256_destination_baseline"
+TOKENIZER_PATH="/mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/src/poredlm/data/stage2_BERT_Encoder/tokenizer-8k.json"
+BASE_SRC="/mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/data/DNA_modifiction/without_modifiction/stage2_BERT_Encoder/train"
+BASE_DST="/mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/data/DNA_modifiction/without_modifiction/stage2_BERT_Encoder/train"
 
 # 对三个数据集分别运行
-for split in validation test train; do
+for split in train; do
     dolma tokens \
         --documents "${BASE_SRC}/${split}/*.gz" \
         --tokenizer.name_or_path "$TOKENIZER_PATH" \
         --destination "${BASE_DST}/${split}" \
-        --dtype "uint32" \
+        --dtype "uint16" \
         --tokenizer.pad_token_id 1 \
         --tokenizer.bos_token_id 2 \
         --tokenizer.eos_token_id 3 \
