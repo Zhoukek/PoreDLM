45a845964df4fc31db869c3703a8f758e9a0074f
zkk <kexuan.zhou8@gmail.com>
2026-06-03 10:29:30 +0800

    add OLMo_DLM #7 (convert_olmo2_to_hf_dlm)

--- src/poredlm/data/stage3_OLMo_DLM/step02_olmo2_latest_to_hf.sh
+++ src/poredlm/data/stage3_OLMo_DLM/step02_olmo2_latest_to_hf.sh
@@ -12,7 +12,7 @@ export CUDA_VISIBLE_DEVICES=0,1
 export WANDB_API_KEY=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
 
 
-python3 /mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/src/poredlm/training/stage3_OLMo_DLM/OLMo/scripts/convert_olmo2_to_hf.py \
+python3 /mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/src/poredlm/training/stage3_OLMo_DLM/OLMo/scripts/convert_olmo2_to_hf_dlm.py \
    --input_dir "/mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/src/poredlm/training/stage3_OLMo_DLM/runs/test_zhou/model/olmo_150m_dlm/latest-unsharded"  \
    --output_dir "/mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/src/poredlm/training/stage3_OLMo_DLM/runs/test_zhou/base"  \
-   --tokenizer_json_path "/mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/src/poredlm/data/stage2_BERT_Encoder/tokenizer-8k.json"  \
+   --tokenizer_json_path "/mnt/zzbnew/rnamodel/zhoukexuan/PoreDLM/src/poredlm/data/stage2_BERT_Encoder/tokenizer-8k.json"
