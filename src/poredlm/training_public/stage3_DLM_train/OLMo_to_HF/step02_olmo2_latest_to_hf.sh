9e7ef46ba6fe6f9f9c6a2d6a631c12aa8fa562b2
zhoukexuan <1280721629@qq.com>
2026-08-03 17:22:07 +0800

    add satge_EX_Waveform_Reconstruction #4 (condition_generate)

--- src/poredlm/training_public/stage3_DLM_train/OLMo_to_HF/step02_olmo2_latest_to_hf.sh
+++ src/poredlm/training_public/stage3_DLM_train/OLMo_to_HF/step02_olmo2_latest_to_hf.sh
@@ -9,11 +9,11 @@ RUN_ROOT=${PROJECT_ROOT}/src/poredlm/training_public/stage3_DLM_train/runs/test
 source ${PROJECT_ROOT}/src/poredlm/training/set_env.sh
 
 export PYTHONPATH=${OLMO_ROOT}:${PROJECT_ROOT}/src:${PYTHONPATH:-}
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
 
 python3 ${OLMO_ROOT}/scripts/convert_olmo2_to_hf_dlm.py \
    --input_dir "${RUN_ROOT}/model/step100000-unsharded"  \
-   --output_dir "${RUN_ROOT}/hf_dlm"  \
+   --output_dir "${RUN_ROOT}/hf_dlm_condition"  \
    --tokenizer_json_path "${PROJECT_ROOT}/src/poredlm/data/stage2_BERT_Encoder/tokenizer-64k.json" \
    --overwrite
